3af4023581b498e2ec88e42bcd2dc9c83b516e90
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2018-11-26 21:48:43 +0100

    :bear: :construction: new Transfer channel
    Step 5: Transfer form and channel

---
+++ app/channels/transfer_channel.rb
@@ -0,0 +1,25 @@
+class TransferChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from(channel_name)
+  end
+
+  def send_transfer(payload)
+    to_account = User.find_by_email(payload['email']).account
+    transfer = Transfer.new(from_account: current_user.account, to_account: to_account, amount: amount)
+    ActionCable.server.broadcast(channel_name, account: render(current_user.account)) if transfer.save
+  end
+
+  private
+
+  def amount(payload)
+    BigDecimal(payload['amount'])
+  end
+
+  def channel_name
+    "transfer_#{current_user.email}"
+  end
+
+  def render(account)
+    ApplicationController.new.helpers.c('account', account: account)
+  end
+end
