d2f50598ee4be8a445070cb3a47525eae30f07a7
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2018-11-26 21:36:26 +0100

    :bear: add transfer form to view
    Step 5: Transfer form and channel

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -3,4 +3,5 @@ import "init";
 import "components/page/page";
 import "components/account/account";
 import "components/balance/balance";
+import "components/transfer-form/transfer-form";
 import "components/transactions/transactions";
